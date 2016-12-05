8cc2a67e58f83c36ca1d6707a8bfb976fe0f18e6
William Chang <william@creativecrew.org>
2016-12-05 12:30:54 -0500

    Update script to configure Microsoft IIS for Sitecore CMS.

--- Sitecore_InstallEverything/Install_Sitecore_ConfigureIIS.ps1
+++ Sitecore_InstallEverything/Install_Sitecore_ConfigureIIS.ps1
@@ -6,7 +6,7 @@ Install Sitecore Configure IIS
 Created by William Chang
 
 Created: 2016-09-09
-Modified: 2016-09-09
+Modified: 2016-12-05
 
 #>
 
@@ -42,48 +42,101 @@ function Get-MicrosoftIisModule {
 }
 
 function Add-MicrosoftIisApplicationPool {
-    <#
-    For debugging.
+    param(
+        [string]$Name,
+        [string]$DotNetVersion
+    )
 
-    Get-ItemProperty IIS:\AppPools\sandbox.org | Format-List
-    Get-ItemProperty IIS:\AppPools\sandbox.org -name processModel
-    #>
+    if (!$DotNetVersion) {
+        $DotNetVersion = 'v4.0'
+    }
 
-    $iisApplicationPoolName = 'sandbox.org'
-    $iisApplicationPoolDotNetVersion = 'v4.0'
-
+    $iisApplicationPoolName = $Name
+    $iisApplicationPoolDotNetVersion = $DotNetVersion
     $iisApplicationPoolFolderPath = 'IIS:\AppPools'
     $iisApplicationPoolPath = Join-Path -Path $iisApplicationPoolFolderPath -ChildPath $iisApplicationPoolName
 
-    Get-ChildItem -Path $iisApplicationPoolFolderPath
+    <#
+    For debugging.
 
-    if (!(Test-Path $iisApplicationPoolName -PathType Container)) {
+    Import-Module WebAdministration
+    Get-ChildItem –Path IIS:\AppPools
+    Get-ItemProperty IIS:\AppPools\sandbox.org | Format-List
+    Get-ItemProperty IIS:\AppPools\sandbox.org -name 'processModel'
+    #>
+
+    Write-Debug ('')
+    Write-Debug ('Application Pool Name : {0}' -f $iisApplicationPoolName)
+    Write-Debug ('Application Pool .NET Version : {0}' -f $iisApplicationPoolDotNetVersion)
+    Write-Debug ('Application Pool Path : {0}' -f $iisApplicationPoolPath)
+    Write-Debug ('')
+
+    if (!(Test-Path $iisApplicationPoolPath)) {
         <#
         Create IIS application pool.
         #>
-        #New-Item –Path $iisApplicationPoolPath
+        New-Item -Path $iisApplicationPoolPath
 
         <#
-        Set IIS application pool Microsoft .NET version.
+        Set IIS application pool .NET version.
         #>
-        #Set-ItemProperty -Path $iisApplicationPoolPath -Name managedRuntimeVersion -Value $iisApplicationPoolDotNetVersion
+        Set-ItemProperty -Path $iisApplicationPoolPath -Name 'managedRuntimeVersion' -Value $iisApplicationPoolDotNetVersion
 
         <#
         Set IIS application pool identity.
 
-        processModel.identityType 2 : NetworkService
-        processModel.identityType 4 : ApplicationPoolIdentity
+        processModel.identityType 2 is NetworkService
+        processModel.identityType 4 is ApplicationPoolIdentity
         #>
-        #Set-ItemProperty -Path $iisApplicationPoolPath -Name processModel.identityType -Value 2
+        Set-ItemProperty -Path $iisApplicationPoolPath -Name 'processModel.identityType' -Value 2
     }
 }
 
 function Add-MicrosoftIisSite {
-    $iisSiteName = 'sandbox.org'
+    param(
+        [string]$Name,
+        [string]$WebrootFolderPath,
+        [string]$ApplicationPoolName
+    )
+
+    if (!$ApplicationPoolName) {
+        $ApplicationPoolName = $Name
+    }
+
+    $iisSiteName = $Name
+    $iisSiteApplicationPoolName = $ApplicationPoolName
+    $iisSitePhysicalFolderPath = $WebrootFolderPath
     $iisSiteFolderPath = 'IIS:\Sites'
     $iisSitePath = Join-Path -Path $iisSiteFolderPath -ChildPath $iisSiteName
 
-    if (!(Test-Path $iisApplicationPoolName)) {
+    $iisSiteBindings = @(
+        @{protocol = 'http';bindingInformation = ':80:local.' + $iisSiteName}
+    )
+
+    <#
+    For debugging.
+
+    Import-Module WebAdministration
+    Get-ChildItem –Path IIS:\Sites
+    #>
+
+    Write-Debug ('')
+    Write-Debug ('Site Name : {0}' -f $iisSiteName)
+    Write-Debug ('Site Application Pool Name : {0}' -f $iisSiteApplicationPoolName)
+    Write-Debug ('Site Physical Path : {0}' -f $iisSitePhysicalFolderPath)
+    Write-Debug ('Site Path : {0}' -f $iisSitePath)
+    Write-Debug ('')
+
+    if (!(Test-Path $iisSitePath)) {
+        <#
+        Create IIS site.
+        #>
+        New-Item -Path $iisSitePath -Bindings $iisSiteBindings -PhysicalPath $iisSitePhysicalFolderPath
+
+        <#
+        Set IIS site application pool.
+        #>
+        Set-ItemProperty -Path $iisSitePath -Name 'applicationPool' -Value $iisSiteApplicationPoolName
     }
 }
 
@@ -95,13 +148,18 @@ function Invoke-Main {
     Write-Output ('')
 
     Write-Output ('')
+    Write-Output ('Debug Preference : {0}' -f $DebugPreference)
+    Write-Output ('')
+
+    Write-Output ('')
     Write-Output ('Hosts File Path : {0}' -f $hostsFilePath)
     Write-Output ('CMS Webroot Folder Path : {0}' -f $cmsWebrootFolderPath)
     Write-Output ('')
 
     Get-MicrosoftIisModule
     #Add-MicrosoftWindowsHosts -HostsPath (Join-Path -Path $currentFolderPath -ChildPath 'hosts') -Hostname 'local.sandbox1.com'
-    Add-MicrosoftIisSite
+    #Add-MicrosoftIisApplicationPool -Name 'site1.com'
+    #Add-MicrosoftIisSite -Name 'site1.com' -WebrootFolderPath $cmsWebrootFolderPath
 }
 
 Invoke-Main
